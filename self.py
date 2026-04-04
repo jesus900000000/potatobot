ba246300c894295919a44ad83e17c2df56d4b5d6
Jesus Gutierrez <jgutierrez9000@gmail.com>
2026-04-03 22:32:33 -0500

    Added a version command to display the current version of PotatoBot, and updated the on_ready event to send a message when the bot is online. Also added a help command to list available commands and their descriptions.

--- self.py
+++ self.py
@@ -6,6 +6,9 @@ import random
 import time
 import asyncio
 
+#VERSION
+version = "4.3.3"
+
 #1. Load token
 load_dotenv() 
 token = os.getenv("token")
@@ -23,14 +26,60 @@ async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('---------------------------')
     channel = bot.get_channel(1486218054265213029)  # channel ID
-    await channel.send('PotatoBot is  online and ready to serve you!')
+    await channel.send('PotatoBot is online and ready to serve you!')
 
 
 #-------COMMANDS---------
+commands = [
+    {
+        "name": "version",
+        "description": "Displays the current version of PotatoBot.",
+        "usage": ".version"
+    },
+    {
+        "name": "add",
+        "description": "Adds two numbers together.",
+        "usage": ".add <number1> <number2>"
+    },
+    {
+        "name": "choose",
+        "description": "Choose from your given options.",
+        "usage": ".choose <option1> <option2> ... <optionN>"
+    },
+    {
+        "name": "repeat",
+        "description": "Spammer muahahah.",
+        "usage": ".repeat <times> [content]"
+    },
+    {
+        "name": "joined",
+        "description": "Says when a member joined.",
+        "usage": ".joined @member"
+    },
+    {
+        "name": "spammer",
+        "description": "Toggles spamming of timestamps every 2 seconds.",
+        "usage": ".spammer <true|false>"
+    },
+    {
+        "name": "cool",
+        "description": "Says if a user is cool. Use subcommand 'bot' to check if the bot is cool.",
+        "usage": ".cool [subcommand]"
+    }
+]
+
+@bot.command()
+async def help(ctx):
+    """Displays a list of available commands and their descriptions."""
+    help_message = "Here are the available commands:\n\n"
+    for cmd in commands:
+        help_message += f"**{cmd['name']}**: {cmd['description']}\nUsage: `{cmd['usage']}`\n\n"
+    await ctx.send(help_message)
+
 @bot.command()
 async def version(ctx):
-    """Adds two numbers together."""
-    await ctx.send('PotatoBot version 4.3.2')
+    """Displays the current version of PotatoBot."""
+    await ctx.send('PotatoBot version' + version)
 
 @bot.command()
 async def add(ctx, left: int, right: int):
